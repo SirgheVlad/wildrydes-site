b9c1b835742ceabbdf1a74049095fe0f6b817c1a
SirgheVlad <vlad.andrei.sirghe@gmail.com>
2025-02-17 12:48:01 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_2LpTJV5YH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '20dp2njc3lbjuo4i7nsdfdcq6f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_tUWsORYoZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3reocaesbogr8ovc58h4ah4gqq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
