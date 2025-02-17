db3c974158afe48035e5468dbb818cc8413363fb
SirgheVlad <vlad.andrei.sirghe@gmail.com>
2025-02-17 12:18:17 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_RbkPtajcK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '51461p96a00mskkcgeo7hpi51k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
